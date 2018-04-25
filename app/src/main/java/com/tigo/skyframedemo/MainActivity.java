8470b9593c32ac216b0fccf0c94485f6473e58e8
xcode126 <xcode126@126.com>
2018-04-25 17:43:46 +0800

    提交测试

--- app/src/main/java/com/tigo/skyframedemo/MainActivity.java
+++ app/src/main/java/com/tigo/skyframedemo/MainActivity.java
@@ -2,6 +2,11 @@ package com.tigo.skyframedemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+/**
+ * Author：sky on 2018/4/25 0020 11:01.
+ * Email：xcode126@126.com
+ * Desc：首页
+ */
 
 public class MainActivity extends Activity {
 
