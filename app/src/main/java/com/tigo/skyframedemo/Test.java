55c9436c6ac34457a1de5d43a5e08d1cb76d50b1
xcode126 <xcode126@126.com>
2018-05-05 12:23:42 +0800

    提交测试

---
+++ app/src/main/java/com/tigo/skyframedemo/Test.java
@@ -0,0 +1,40 @@
+package com.tigo.skyframedemo;
+
+import java.util.List;
+
+/**
+ * Author：sky on 2018/5/5 0005 12:22.
+ * Email：xcode126@126.com
+ * Desc：
+ */
+
+public class Test {
+    public int total;
+    public List<MomentListBean> momentList;
+
+    public static class MomentListBean {
+        public String cretime;
+        public int customercode;
+        public String customername;
+        public int evaluate;
+        public int evaluatenum;
+        public int fanscount;
+        public int follow;
+        public int forward;
+        public int forwardid;
+        public int forwardnum;
+        public String forwardtitle;
+        public String headimg;
+        public int id;
+        public String imgs;
+        public int momentid;
+        public int praise;
+        public int praisenum;
+        public String title;
+        public String type;
+        public String videolength;
+        public String videosize;
+        public String videourl;
+        public int views;
+    }
+}
